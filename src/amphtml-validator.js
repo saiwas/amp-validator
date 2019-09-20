00d24c67f3ef765bf251c46a2d24c8c329410c18
Johnny <jzhang@xogrp.com>
2019-09-20 14:50:52 +0800

    Add amphtml-validator

---
+++ src/amphtml-validator.js
@@ -0,0 +1,21 @@
+'use strict';
+const amphtmlValidator = require('amphtml-validator');
+
+module.exports = async (htmlString) => {
+  const validator = await amphtmlValidator.getInstance()
+  const result = validator.validateString(htmlString)
+  const outPutMsg = []
+
+  for (let ii = 0; ii < result.errors.length; ii++) {
+    const error = result.errors[ii];
+    let msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+    if (error.specUrl !== null) {
+      msg += ' (see ' + error.specUrl + ')';
+    }
+    outPutMsg.push({
+        `${error.severity}`: msg
+    })
+  }
+
+  return outPutMsg
+}
